2286d0c7d3c8facf7b702f65426d6b398308565a
sdias22 <78386630+sdias22@users.noreply.github.com>
2022-08-23 20:55:58 -0300

    Delete ast directory

--- ast/AbstractCommand.java
+++
@@ -1,7 +0,0 @@
-package ast;
-
-public abstract class AbstractCommand {
-    public abstract void executa();
-	public abstract String generateJavaCode();
-    public abstract String generateCCode();
-}
